861ae01ae1e426d1c0dc535c662be959b01c7e2f
Sarthak Sethi <sarthaksethigithub@gmail.com>
2020-05-23 20:07:50 +0530

    added gallery

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const activeImage = document.querySelector(".active-image img");
+const images = document.querySelectorAll(".img-card img");
+const activeDesc = documnet.querySelector(".active-img-desc");
 
+// listeners
+ images.forEach(image => (image.addEventListener("click",changeImage)));
+
+ function changeImage(e) {
+   activeImage.src = e.target.src;
+   activeDesc.innerHTML = e.target.nextElementSibling.innerText;
+ }
